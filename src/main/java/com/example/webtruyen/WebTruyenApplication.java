729a05c8414f88606a1e2c7bea154bbaa46ff5fa
Kiều Khiêm <khiem171222@gmail.com>
2022-09-27 19:56:57 +0700

    first commit

---
+++ src/main/java/com/example/webtruyen/WebTruyenApplication.java
@@ -0,0 +1,12 @@
+package com.example.webtruyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTruyenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebTruyenApplication.class, args);
+    }
+
+}
